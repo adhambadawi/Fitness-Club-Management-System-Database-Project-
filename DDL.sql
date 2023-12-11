9e97933a1bbd3f6b75682c67e2ac64db655452a1
adhambadawi <112950215+adhambadawi@users.noreply.github.com>
2023-12-10 22:43:01 -0500

    Add files via upload

---
+++ DDL.sql
@@ -0,0 +1,116 @@
+-- Table for Members
+CREATE TABLE Members (
+    MemberID INT PRIMARY KEY,
+    FName VARCHAR(255),
+    LName VARCHAR(255),
+    DoB DATE,
+    Address TEXT,
+    HealthMetrics TEXT,
+    FitnessGoal TEXT,
+    ClassSubscription INT,
+    LoyaltyPoints INT
+);
+
+-- Table for Trainers
+CREATE TABLE Trainers (
+    TrainerID INT PRIMARY KEY,
+    FName VARCHAR(255),
+    LName VARCHAR(255),
+    DoB DATE,
+    PrivateTrainingMembers INT -- Assuming this is a count; adjust if it's a foreign key or other type of field
+);
+
+-- Table for Classes
+CREATE TABLE Classes (
+    ClassID INT PRIMARY KEY,
+    Name VARCHAR(255),
+    Description TEXT
+);
+
+-- Table for Rooms
+CREATE TABLE Rooms (
+    RoomID INT PRIMARY KEY,
+    Name VARCHAR(255),
+    RoomType VARCHAR(255),
+    Availability BOOLEAN
+);
+
+-- Table for Equipment
+CREATE TABLE Equipment (
+    EquipmentID INT PRIMARY KEY,
+    EquipmentType VARCHAR(255),
+    Description TEXT
+);
+
+-- Table for Personalized Dashboard
+CREATE TABLE PersonalizedDashboards (
+    DashboardID INT PRIMARY KEY,
+    ExerciseRoutine TEXT,
+    FitnessAchievement TEXT,
+    HealthStatistics TEXT,
+    ProgressNotes TEXT,
+    MemberID INT,
+    FOREIGN KEY (MemberID) REFERENCES Members(MemberID)
+);
+
+-- Table for Personal Training Sessions
+CREATE TABLE PersonalTrainingSessions (
+    SessionID INT PRIMARY KEY,
+    SessionSchedule DATE,
+    Time TIME,
+    Duration INT, -- Assuming duration is in minutes; adjust as necessary
+    MemberID INT,
+    FOREIGN KEY (MemberID) REFERENCES Members(MemberID)
+);
+
+-- Table for Billing
+CREATE TABLE Billing (
+    TransactionID INT PRIMARY KEY,
+    Amount DECIMAL(10, 2),
+    Date DATE,
+    PaymentMethod VARCHAR(255),
+    Service VARCHAR(255),
+    MemberID INT,
+    FOREIGN KEY (MemberID) REFERENCES Members(MemberID)
+);
+
+-- Table for Administrative Staff
+CREATE TABLE AdministrativeStaff (
+    StaffID INT PRIMARY KEY,
+    FName VARCHAR(255),
+    LName VARCHAR(255),
+    DoB DATE,
+    Role VARCHAR(255)
+);
+
+-- Associative table for the many-to-many relationship between Members and Classes
+CREATE TABLE Members_Classes (
+    MemberID INT,
+    ClassID INT,
+    PRIMARY KEY (MemberID, ClassID),
+    FOREIGN KEY (MemberID) REFERENCES Members(MemberID),
+    FOREIGN KEY (ClassID) REFERENCES Classes(ClassID)
+);
+
+-- Associative table for the relationship between Classes and Rooms
+CREATE TABLE Classes_Rooms (
+    ClassID INT,
+    RoomID INT,
+    PRIMARY KEY (ClassID, RoomID),
+    FOREIGN KEY (ClassID) REFERENCES Classes(ClassID),
+    FOREIGN KEY (RoomID) REFERENCES Rooms(RoomID)
+);
+
+-- Associative table for the relationship between Rooms and Equipment
+CREATE TABLE Rooms_Equipment (
+    RoomID INT,
+    EquipmentID INT,
+    PRIMARY KEY (RoomID, EquipmentID),
+    FOREIGN KEY (RoomID) REFERENCES Rooms(RoomID),
+    FOREIGN KEY (EquipmentID) REFERENCES Equipment(EquipmentID)
+);
+
+-- Note: This schema assumes that the 'n' and '1' indicators in the ER model represent one-to-many relationships, which are handled by foreign keys in the respective tables.
+-- The associative tables handle the many-to-many relationships.
+-- You may need to adjust the types and add more fields such as created_at, updated_at, etc., as per your requirements.
+-- Also, consider adding indexes on foreign keys for performance improvements.
---
+++ DML.sql
@@ -0,0 +1,76 @@
+--Insert a new member into the Members table:
+INSERT INTO Members (MemberID, FName, LName, DoB, Address, HealthMetrics, FitnessGoal, ClassSubscription, LoyaltyPoints)
+VALUES (1, 'John', 'Doe', '1990-01-01', '123 Main St', 'Good', 'Lose Weight', 1, 100);
+
+
+--Update the address of a member in the Members table:
+UPDATE Members
+SET Address = '456 Elm St'
+WHERE MemberID = 1;
+
+
+--Delete a member from the Members table:
+DELETE FROM Members
+WHERE MemberID = 1;
+Select all members from the Members table:
+SELECT * FROM Members;
+
+
+--Insert a new class into the Classes table:
+INSERT INTO Classes (ClassID, Name, Description)
+VALUES (1, 'Yoga Beginners', 'A class for beginners to learn yoga basics.');
+
+
+--Enroll a member in a class:
+INSERT INTO Members_Classes (MemberID, ClassID)
+VALUES (1, 1);
+
+
+--Find all classes a member is enrolled in:
+SELECT c.ClassID, c.Name, c.Description
+FROM Classes c
+JOIN Members_Classes mc ON c.ClassID = mc.ClassID
+WHERE mc.MemberID = 1;
+
+
+--List all equipment in a given room:
+SELECT e.EquipmentID, e.EquipmentType, e.Description
+FROM Equipment e
+JOIN Rooms_Equipment re ON e.EquipmentID = re.EquipmentID
+WHERE re.RoomID = 1;
+
+
+--Record a new personal training session:
+INSERT INTO PersonalTrainingSessions (SessionID, SessionSchedule, Time, Duration, MemberID)
+VALUES (1, '2023-12-10', '10:00', 60, 1);
+
+
+--Update the duration of a personal training session:
+UPDATE PersonalTrainingSessions
+SET Duration = 90
+WHERE SessionID = 1;
+
+
+--List all members with a particular fitness goal:
+SELECT MemberID, FName, LName
+FROM Members
+WHERE FitnessGoal = 'Lose Weight';
+
+
+--Calculate the total amount billed to a member:
+SELECT SUM(Amount) as TotalBilled
+FROM Billing
+WHERE MemberID = 1;
+
+
+--Find the availability of all rooms:
+SELECT RoomID, Name, Availability
+FROM Rooms
+WHERE Availability = TRUE;
+
+
+--List the schedule of classes for a specific room:
+SELECT cl.Name, cl.Description
+FROM Classes cl
+JOIN Classes_Rooms cr ON cl.ClassID = cr.ClassID
+WHERE cr.RoomID = 1;
